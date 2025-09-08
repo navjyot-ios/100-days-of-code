e36f4890f33b2bd92445a830976e794962efcf9f
navjyot-ios <navjyotsingh5911@gmail.com>
2025-09-08 14:38:35 +0530

    ques5

---
+++ 590026061-navjyot-003-q5.c
@@ -0,0 +1,12 @@
+
+// program to convert celcius to fahrenheit
+#include <stdio.h>
+ int main(){
+    float c;
+    float f;
+    printf("Enter Celcius : ");
+    scanf("%f",&c);
+    f=(c*9/5)+32;
+    printf("Fahrenheit = %f\n",f);
+
+ }
